c0cf3749c783df4529767277693056f5db5a2137
Ian Gabriel Calica <iggc654@gmail.com>
2026-08-26 13:15:10 +0800

    Added init query

---
+++ scripts/init_query.sql
@@ -0,0 +1,23 @@
+-- Create  Database 'DataWarehouse'
+
+USE master;
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
